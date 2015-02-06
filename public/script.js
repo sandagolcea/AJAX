7dd525a3301e65386aa86923b2bbd5a07b3ba291
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-06 16:51:07 +0000

    Adds users with info from github on button press

--- public/script.js
+++ public/script.js
@@ -1,20 +1,16 @@
+function addUser(user) {
+  $.get('https://api.github.com/users/' + user + '?access_token=5880f9e03c22d35482a61a9053bd9308cd600752',function(user){
+    var newProfile = Mustache.render($('#profile-template').html(), user);
+    $('.profile-container').append(newProfile);
+  });
+}
+
+
 $(document).ready(function(){
-  $.get('https://api.github.com/users/sandagolcea',function(user){
-    
-      var newProfile = Mustache.render($('#profile-template').html(), user);
-      $('.profile-container').append(newProfile);
-  });
-
-  $.get('https://api.github.com/users/tansaku',function(user){
-    
-      var newProfile = Mustache.render($('#profile-template').html(), user);
-      $('.profile-container').append(newProfile);
-  });
-
-  $.get('https://api.github.com/users/jaircazarin',function(user){
-    
-      var newProfile = Mustache.render($('#profile-template').html(), user);
-      $('.profile-container').append(newProfile);
-  });
-
+  addUser('sandagolcea');
+  $('#add_profile').on('submit', function(event){
+    event.preventDefault();
+    console.log('form submitted');
+    addUser($('#username').val());
+  })  
 });
